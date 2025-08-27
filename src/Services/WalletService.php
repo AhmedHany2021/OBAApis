9abb861cb3c42f592f5f997fffe62e4648960425
ahmedhanyshah <ahmed.hany@shahbandr.team>
2025-08-27 04:19:49 +0300

    wallet starter

---
+++ src/Services/WalletService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OBA\APIsIntegration\Services;
+use WP_REST_Request;
+use WP_REST_Response;
+use WP_Error;
+
+class WalletService
+{
+    public function get_wallet_balance(WP_REST_Request $request)
+    {
+        
+    }
+}
