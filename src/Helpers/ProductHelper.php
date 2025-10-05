434408e91b45bef3d0b5fbba127682a54032157a
Mohamed Hesham <59764426+MOHAMEDHESMAM8@users.noreply.github.com>
2025-10-05 10:56:21 +0300

    attribute names

--- src/Helpers/ProductHelper.php
+++ src/Helpers/ProductHelper.php
@@ -93,7 +93,7 @@ class ProductHelper {
 
                 $variations_data[] = [
                     'id'            => $variation_id,
-                    'attributes'    => $variation_obj->get_attributes(),
+                    'attributes'    => self::format_variation_attributes($variation_obj),
                     'regular_price' => (float) $variation_obj->get_regular_price(),
                     'sale_price'    => $variation_obj->get_sale_price() ? (float) $variation_obj->get_sale_price() : null,
                     'stock_status'  => $variation_obj->get_stock_status(),
@@ -111,5 +111,59 @@ class ProductHelper {
         }
 
         return $product_data;
+    }
+
+    /**
+     * Format variation attributes with display names and values
+     *
+     * @param \WC_Product_Variation $variation Variation object.
+     * @return array
+     */
+    private static function format_variation_attributes($variation)
+    {
+        $formatted_attributes = [];
+        $attributes = $variation->get_attributes();
+
+        foreach ($attributes as $attribute_name => $attribute_value) {
+            // Check if this is a taxonomy-based attribute (starts with 'pa_')
+            if (strpos($attribute_name, 'pa_') === 0) {
+                // Get the taxonomy name (attribute name is the taxonomy)
+                $taxonomy = $attribute_name;
+                
+                // Get the attribute term object
+                $term = get_term_by('slug', $attribute_value, $taxonomy);
+                
+                if ($term && !is_wp_error($term)) {
+                    // Get the attribute taxonomy object for display name
+                    $attribute_taxonomy_id = wc_attribute_taxonomy_id_by_name(str_replace('pa_', '', $attribute_name));
+                    $attribute_taxonomy = wc_get_attribute($attribute_taxonomy_id);
+                    $display_name = $attribute_taxonomy ? $attribute_taxonomy->name : ucwords(str_replace('-', ' ', str_replace('pa_', '', $attribute_name)));
+                    
+                    $formatted_attributes[$display_name] = [
+                        'slug' => $attribute_value,
+                        'value' => $term->name,
+                        'term_id' => $term->term_id
+                    ];
+                } else {
+                    // Fallback when term not found
+                    $display_name = ucwords(str_replace('-', ' ', str_replace('pa_', '', $attribute_name)));
+                    $formatted_attributes[$display_name] = [
+                        'slug' => $attribute_value,
+                        'value' => $attribute_value,
+                        'term_id' => null
+                    ];
+                }
+            } else {
+                // For non-taxonomy attributes (custom attributes)
+                $display_name = ucwords(str_replace('-', ' ', $attribute_name));
+                $formatted_attributes[$display_name] = [
+                    'slug' => $attribute_value,
+                    'value' => $attribute_value,
+                    'term_id' => null
+                ];
+            }
+        }
+
+        return $formatted_attributes;
     }
 }
